eb60f74e6afcd5bb89a092ce51135ef1cf36fbf6
Shilin Victor <chrono.monochrome@gmail.com>
2015-10-10 21:52:03 +0300

    Revert "BufferQueueProducer: add detachNextBuffer"
    This reverts commit d9822a3843017444364899afc3c23fb5be6b9cb9.
    
    Conflicts:
    	libs/gui/BufferQueueProducer.cpp
    
    Change-Id: I995c86fc96a8650ddce109b6b4cd1f77b6a9397b

--- include/gui/BufferQueueProducer.h
+++ include/gui/BufferQueueProducer.h
@@ -99,10 +99,6 @@ public:
     // See IGraphicBufferProducer::detachBuffer
     virtual status_t detachBuffer(int slot);
 
-    // See IGraphicBufferProducer::detachNextBuffer
-    virtual status_t detachNextBuffer(sp<GraphicBuffer>* outBuffer,
-            sp<Fence>* outFence);
-
     // See IGraphicBufferProducer::attachBuffer
     virtual status_t attachBuffer(int* outSlot, const sp<GraphicBuffer>& buffer);
 
