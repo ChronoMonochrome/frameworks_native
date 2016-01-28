ec34d56239415d0cc42191896f88fbf27b0b5254
Shilin Victor <chrono.monochrome@gmail.com>
2016-01-28 16:57:21 +0700

    Revert "reinstate black-screenshot debugging code"
    This reverts commit d555684cb36dfb959694db76962e570184f98838.
    
    Change-Id: Ic806b9fe3c10babb858239406038154f1adec5f7

--- services/surfaceflinger/RenderEngine/RenderEngine.h
+++ services/surfaceflinger/RenderEngine/RenderEngine.h
@@ -70,7 +70,6 @@ public:
     void disableScissor();
     void genTextures(size_t count, uint32_t* names);
     void deleteTextures(size_t count, uint32_t const* names);
-    void readPixels(size_t l, size_t b, size_t w, size_t h, uint32_t* pixels);
 
     class BindImageAsFramebuffer {
         RenderEngine& mEngine;
@@ -106,6 +105,8 @@ public:
     // queries
     virtual size_t getMaxTextureSize() const = 0;
     virtual size_t getMaxViewportDims() const = 0;
+
+
 
     EGLContext getEGLContext() const;
 };
