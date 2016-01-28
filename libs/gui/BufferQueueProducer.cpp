dadf204662042cb8da426b74b1931ee7162af171
Shilin Victor <chrono.monochrome@gmail.com>
2016-01-28 12:55:18 +0700

    Revert "BufferQueue: Set attached buffers as requested"
    This reverts commit 2443c7903c6910b57bcc615a4bf2e60068c15dbd.

--- libs/gui/BufferQueueProducer.cpp
+++ libs/gui/BufferQueueProducer.cpp
@@ -434,7 +434,6 @@ status_t BufferQueueProducer::attachBuffer(int* outSlot,
     mSlots[*outSlot].mBufferState = BufferSlot::DEQUEUED;
     mSlots[*outSlot].mEglFence = EGL_NO_SYNC_KHR;
     mSlots[*outSlot].mFence = Fence::NO_FENCE;
-    mSlots[*outSlot].mRequestBufferCalled = true;
 
     return returnFlags;
 }
